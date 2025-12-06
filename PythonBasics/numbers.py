3c8a88bf9cbc7ee705e465c462b88d8c0601e74f
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2025-12-05 19:27:36 -0600

    add code related to topic basic python

---
+++ PythonBasics/numbers.py
@@ -0,0 +1,22 @@
+# int --> integer
+# float --> floating points number
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+print(type(5.00001))
+print(type(20 + 1.1))
+print(type(9.9 + 1.1))
+
+# 2 to the power of 2
+print (2 ** 2)
+
+# round it (floor division), take the integer part
+print(2 // 4)
+print(3 // 4)
+print(5 // 4)
+
+# get the remainder using %, modulo
+print(5 % 4)
+print(6 % 4)
