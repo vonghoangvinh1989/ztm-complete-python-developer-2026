3e5400b0597e8e27735642a4b260390f50195706
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2025-12-13 21:57:07 -0600

    add code PythonBasics2

---
+++ PythonBasics2/conditional_logic.py
@@ -0,0 +1,9 @@
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+
+print('okoko')
